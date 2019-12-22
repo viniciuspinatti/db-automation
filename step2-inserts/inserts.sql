5163975272bf19d83ddf734eb121b661e8833b80
Vinicius Pinatti <viniciuspinatti@hotmail.com>
2019-12-22 14:43:47 -0300

    Some inserts

---
+++ step2-inserts/inserts.sql
@@ -0,0 +1,19 @@
+/*
+    Some inserts. Enter the information you want.
+*/
+
+insert into automations.person(name) values('Scorpion');
+insert into automations.person(name) values('Sub-Zero');
+insert into automations.person(name) values('Noob Saibot');
+insert into automations.person(name) values('Jade');
+insert into automations.person(name) values('Raiden');
+insert into automations.person(name) values('Liu Kang');
+insert into automations.person(name) values('Johnny Cage');
+insert into automations.person(name) values('Sonya Blade');
+insert into automations.person(name) values('Reptile');
+insert into automations.person(name) values('Shao Kahn');
+
+select
+	*
+from
+	automations.person;
